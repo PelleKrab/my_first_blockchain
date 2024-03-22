da64271431fc5775c657dfaed4132266cad6c947
pellekrab <78560773+PelleKrab@users.noreply.github.com>
2024-03-22 16:05:24 -0600

    Refactor integration tests and fix formatting

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,12 +1,14 @@
+use my_first_blockchain::blockchain::Block;
 use my_first_blockchain::blockchain::Blockchain;
 use std::time::{SystemTime, UNIX_EPOCH};
-use my_first_blockchain::blockchain::Block;
 
 #[cfg(test)]
-mod tests { 
+mod tests {
 
-
-    use my_first_blockchain::{utils::{recover_public_key, sign_transaction, public_key_to_address, generate_key_pair}, transaction::Transaction};
+    use my_first_blockchain::{
+        transaction::Transaction,
+        utils::{generate_key_pair, public_key_to_address, recover_public_key, sign_transaction},
+    };
     use secp256k1::Message;
     use sha3::{Digest, Keccak256};
 
@@ -29,7 +31,7 @@ mod tests {
     fn test_is_chain_valid() {
         let mut blockchain = Blockchain::new();
         let data = "Test Block".to_string();
-        
+
         for _ in 0..5 {
             blockchain.mine_block(data.clone());
         }
@@ -50,34 +52,46 @@ mod tests {
         let new_block = &blockchain.get_chain()[1];
         assert_eq!(new_block.get_index(), 1);
         assert_eq!(new_block.get_data(), data);
-        assert_eq!(new_block.get_previous_hash(), blockchain.get_chain()[0].get_hash());
+        assert_eq!(
+            new_block.get_previous_hash(),
+            blockchain.get_chain()[0].get_hash()
+        );
         assert_ne!(new_block.get_hash(), "0");
         assert_ne!(new_block.get_nonce(), 0);
     }
 
     #[test]
-    fn test_tran    fn test_transaction_sender_valid() {
-        saction_sender_valid() {
-        let (prikey, pubkey) = generate_key_pair();
+    fn test_transaction_sender_valid() {
+        #[test]
+        fn test_transaction_sender_valid() {
+            let (prikey, pubkey) = generate_key_pair();
 
-    let sender = public_key_to_address(&pubkey);
-    let receiver = "0x5678".to_string();
-    let amount = 100;
-    let nonce = 0;
+            let sender = public_key_to_address(&pubkey);
+            let receiver = "0x5678".to_string();
+            let amount = 100;
+            let nonce = 0;
 
-    let signature = sign_transaction(prikey, sender.clone(), receiver.clone(), amount, nonce);
+            let signature =
+                sign_transaction(prikey, sender.clone(), receiver.clone(), amount, nonce);
 
-    let message = format!("{}{}{}{}", sender, receiver, amount, nonce);
-    let message_hash = Keccak256::digest(message.as_bytes());
-    let message_hash = Message::from_digest_slice(&message_hash).expect("Failed to convert message hash");
+            let message = format!("{}{}{}{}", sender, receiver, amount, nonce);
+            let message_hash = Keccak256::digest(message.as_bytes());
+            let message_hash =
+                Message::from_digest_slice(&message_hash).expect("Failed to convert message hash");
 
+            let transaction = Transaction::new(
+                sender.clone(),
+                receiver.clone(),
+                amount,
+                nonce,
+                signature.clone(),
+            );
 
-    let transaction = Transaction::new(sender.clone(), receiver.clone(), amount, nonce, signature.clone());
+            let recovered_pubkey = recover_public_key(&message_hash, &signature);
 
-    let recovered_pubkey = recover_public_key(&message_hash, &signature);
+            assert_eq!(recovered_pubkey, Ok(pubkey));
 
-    assert_eq!(recovered_pubkey, Ok(pubkey));
-    
-    assert_eq!(transaction.verify_signature(), true);
+            assert_eq!(transaction.verify_signature(), true);
+        }
     }
 }
