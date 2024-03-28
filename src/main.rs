43ce4218126bee5e4b16f1185470abdb8f229ec0
pellekrab <78560773+PelleKrab@users.noreply.github.com>
2024-03-28 16:13:48 -0600

    New main.rs showcase

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,55 @@
-use my_first_blockchain::{blockchain::Blockchain, utils::recover_public_key, utils:: generate_key_pair};
 use my_first_blockchain::transaction;
-use libp2p;
-use secp256k1::{Secp256k1, PublicKey, SecretKey, rand, Message};
-use sha3::{Digest, Keccak256};
+use my_first_blockchain::utils::{public_key_to_address, sign_transaction};
+use my_first_blockchain::{blockchain::Blockchain, utils::generate_key_pair};
 use transaction::Transaction;
-use my_first_blockchain::utils::{sign_transaction, public_key_to_address};
-
 
 fn main() {
-    // let mut blockchain = Blockchain::new();
-
-    // blockchain.mine_block("Test");
-    let (prikey, pubkey) = generate_key_pair();
-
-    let sender = public_key_to_address(&pubkey);
-    let receiver = "0x5678".to_string();
-    let amount = 100;
-    let nonce = 0;
-
-    let signature1 = sign_transaction(prikey, sender.clone(), receiver.clone(), amount, nonce);
-    let signature2 = sign_transaction(prikey, sender.clone(), receiver.clone(), amount+1, nonce+1);
-
-
-    let message = format!("{}{}{}{}", sender, receiver, amount, nonce);
-    let message_hash = Keccak256::digest(message.as_bytes());
-    let message_hash = Message::from_digest_slice(&message_hash).expect("Failed to convert message hash");
-
-
-    let transaction1 = Transaction::new(sender.clone(), receiver.clone(), amount, nonce, signature1.clone());
-    let transaction2 = Transaction::new(sender.clone(), receiver.clone(), amount+1, nonce+1, signature2.clone());
-
-    let recovered_pubkey = recover_public_key(&message_hash, &signature1);
-
-    assert_eq!(recovered_pubkey, Ok(pubkey));
-    
-    println!("Public key: {:?}", pubkey);
-    println!("Recovered public key: {:?}", recovered_pubkey);
-    assert_eq!(transaction1.verify_signature(), true);
-
+    // Create a new blockchain
     let mut blockchain = Blockchain::new();
-    let data = vec!(transaction1,transaction2);
 
-    let result = blockchain.mine_block(&data);
+    // Generate some key pairs for the transactions
+    let (prikey1, pubkey1) = generate_key_pair();
+    let (_, pubkey2) = generate_key_pair();
 
-    assert_eq!(result, true);
+    // Create some transactions
+    let transaction1 = Transaction::new(
+        public_key_to_address(&pubkey1),
+        public_key_to_address(&pubkey2),
+        10,
+        0,
+        sign_transaction(
+            prikey1,
+            public_key_to_address(&pubkey1),
+            public_key_to_address(&pubkey2),
+            10,
+            0,
+        ),
+    );
+    println!("Difficulity is: {}", blockchain.get_difficulty());
 
-    assert_eq!(blockchain.is_chain_valid(), true);
+    // Mine a block with this transaction
+    blockchain.mine_block(&vec![transaction1.clone()]);
 
-    println!("New test:");
-    let transaction_to_check = Transaction::new(sender.clone(), receiver.clone(), amount, nonce, signature1.clone());
-    let is_included = blockchain.check_transaction_validity(&transaction_to_check);
+    // Check if the blockchain is valid
+    if blockchain.is_chain_valid() {
+        println!("The blockchain is valid.");
+    } else {
+        println!("The blockchain is not valid.");
+    }
 
-    println!("Is transaction included: {}", is_included);
+    // Verify the transaction's signature
+    let transaction = &blockchain.get_chain()[1].get_data_raw()[0];
+    if transaction.verify_signature() {
+        println!("The transaction's signature is valid.");
+    } else {
+        println!("The transaction's signature is not valid.");
+    }
 
-    
-    // Quick frontend and add function that searches the chain for accounts
-    // Mining transactions
-    //Very simple double spend check. give money each time a block is mined.
+    // Check the transaction's inclusion in the Merkle tree
+    if blockchain.check_transaction_validity(&transaction1) {
+        println!("The transaction is included in the Merkle tree.");
+    } else {
+        println!("The transaction is not included in the Merkle tree.");
+    }
 }
-
+//derive clone stuff
